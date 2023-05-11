d611bca373021ee1d6b6fd697fc0a344ed33b9b6
Kittycraft0 <iwbmo05@gmail.com>
2023-05-11 11:25:06 -0500

    beginning implimentation of gravity methods

---
+++ src/math/gravitionmath.h
@@ -0,0 +1,9 @@
+// 5/11/2023
+#ifndef GRAVITYMATH_H
+#define GRAVITYMATH_H
+//the gravity math!
+//TODO: find old name I used
+//and now I will soon be taking the AP Physics 1 test... Hope I do well!
+void gravity();
+
+#endif //GRAVITYMATH_H
