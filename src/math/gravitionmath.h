300117d26524ce6d0df26a09fab7bbcccbd007c1
Kittycraft0 <iwbmo05@gmail.com>
2023-05-11 11:26:57 -0500

    What

--- src/math/gravitionmath.h
+++ src/math/gravitionmath.h
@@ -1,9 +1,11 @@
 // 5/11/2023
 #ifndef GRAVITYMATH_H
 #define GRAVITYMATH_H
-//the gravity math!
-//TODO: find old name I used
-//and now I will soon be taking the AP Physics 1 test... Hope I do well!
+// The gravity math!
+// TODO: find old name I used
+// and now I will soon be taking the AP Physics 1 test... Hope I do well!
+// Without my tungsten cube... Hahahahaha
+// Oh... The gravity method in the Javascript 3d engine was just called "gravity"... Wow...
 void gravity();
 
 #endif //GRAVITYMATH_H
