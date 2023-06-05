1cc92285f67c2ea1186ebb2e46646f91c6ea7809
Kittycraft0 <iwbmo05@gmail.com>
2023-06-05 15:08:45 -0500

    Cleaned up script

--- src/render/render2d.cpp
+++ src/render/render2d.cpp
@@ -1,32 +1,26 @@
 // 6/5/2023
 // render2d.cpp
 #include "render2d.h"
-// no semicolons after the incldue
 
 // 6/5/2023
-// render all of the objects as circles regardless of model
+// Render all of the objects as circles regardless of model
 void render2dAsCircles(Data* data){
+    // For every object
     for(ObjectNd* obj:data->objects){
+        // Initialize the rendered object
         sf::RectangleShape sfModel;
+        // Set the size
         sfModel.setSize(sf::Vector2f(10.f,10.f));
+        // Set the color
         sfModel.setFillColor(sf::Color::Green);
+        // Set the position
         sfModel.setPosition(sf::Vector2f(obj->p[0],obj->p[1]));
-        //sfModel.setOrigin(-(data->window->getSize().x)/64,0);
-        //std::cout<<data->window->getSize().x;
-        // wow an unsigned int divided by a negative number is 0 
-        // weird but interesting i guess
-        //std::cout<<(data->window->getSize().x)/(-2)<<" ";
-        //std::cout<<((int)data->window->getSize().x)/(-2)<<" ";
-        //sfModel.setOrigin(-100,0);
+        // Set the origin
         sfModel.setOrigin(
             -(int)data->window->getSize().x/2,
             -(int)data->window->getSize().y/2
         );
+        // Draw the object
         data->window->draw(sfModel);
-        
-        
     }
-    // prevent memory leaks???
-    //delete data;
-    // hahaha no that causes a seg fault!
 }
