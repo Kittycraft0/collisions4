e0ab9477fcd7f5f20c4e4173042a655af441fba8
Kittycraft0 <iwbmo05@gmail.com>
2023-06-20 20:28:28 -0500

    Terminal windows open without error!

--- projects/helloworld/helloworld.cpp
+++ projects/helloworld/helloworld.cpp
@@ -2,7 +2,12 @@
 // 6/19/2023
 #include <iostream>
 
+// 6/20/2023
+#include <conio.h>
+
 int main(){
     std::cout<<"HELLO WORLD...";
+    std::cout<<"\nPress any key to exit...";
+    getch();
     return 0;
 }
--- projects/invSqrtTest/invsqrttest.cpp
+++ projects/invSqrtTest/invsqrttest.cpp
@@ -3,13 +3,18 @@
 #include <iostream>
 #include "../../src/math/invsqrt.h"
 
+// 6/20/2023
+#include <conio.h>
+
 // 5/9/2023
 // testing the invsqrt.cpp thing...
 
 // remember the capital S... in invSqrt();...
 
 int main(){
-    std::cout<<"HELLO WORLD...";
+    std::cout<<"HELLO WORLD...\n";
     std::cout<<invSqrt((float)(4.0f)); //0.5
+    std::cout<<"\nPress any key to exit...";
+    getch();
     return 0;
 }
