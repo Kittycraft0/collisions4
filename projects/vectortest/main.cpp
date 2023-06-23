f99748f5aa6e421d0577787bb6e5334b84f7a139
Kittycraft0 <iwbmo05@gmail.com>
2023-06-22 20:40:02 -0500

    Isolated problem

---
+++ projects/vectortest/main.cpp
@@ -0,0 +1,20 @@
+// 4/25/2023 - I am now 18! Wow! Not a factorial. Coding at 6:19 PM CST.
+
+//finally works alright! -3/13/2023
+//now to finally start coding after, what, 3 months?. finally...
+// 6/22/2023 YOUUUUU WHYYYY AAAAAAAAAAAAAA ABOVE REALLY!? WOOOOOOW HOW COULD YOU... THIS STUPID STUFF... IT'S LIKE MORE THAN 3 MONTHS FROM THEN
+// AND LOOK AT ME NOW... AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+#include <iostream>
+#include <list>
+#include <random>
+#include <cmath>
+//#include "../../src/src.h"
+
+
+int main()
+{
+    std::cout<<"Ran test!\n";
+    std::vector<int> error;
+}
+
+
